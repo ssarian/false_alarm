5c6ee0632f3d802d7b86baaf0d5ebae50b2dec2e
nathan <nsgreene@uh.edu>
2016-11-05 15:03:47 -0500

    added routes to main login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :homes
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'homes#Home'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
