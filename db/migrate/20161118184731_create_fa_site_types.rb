a9a0ac50f771925d1edb9d0e3ce3154f24f57ee2
nathan <nsgreene@uh.edu>
2016-11-19 16:25:19 -0600

    trying 2 fix logo

---
+++ db/migrate/20161118184731_create_fa_site_types.rb
@@ -0,0 +1,10 @@
+class CreateFaSiteTypes < ActiveRecord::Migration
+  def change
+    create_table :fa_site_types do |t|
+      t.text :description
+      t.char :siteCode
+
+      t.timestamps null: false
+    end
+  end
+end
