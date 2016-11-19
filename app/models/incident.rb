7921dc0243f8f69e40cc94966a761247482bfbfc
nathan <nsgreene@uh.edu>
2016-11-19 16:12:03 -0600

    tables n shit

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,3 +1,13 @@
 class Incident < ActiveRecord::Base
   has_many :incident_alarms
+  validates :incidentNum, :presence => true
+  validates :incidentDateTime, :presence => true
+  validates :permitNum, :presence => true
+  validates :address, :presence => true
+            validates :city, format: { with: /\A[a-zA-Z]+\z/}, :presence => true
+            validates :state, format: { with: /\A[a-zA-Z]+\z/}, length: { maximum: 2, minimum: 2}, :presence => true
+  validates :zip, numericality: true, length: {maximum:5, minimum: 5}, :presence => true
 end
+
+
+
