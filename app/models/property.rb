0749791f00f3d8cdd3d58a88f21e0d6cd4bb4c54
nathan <nsgreene@uh.edu>
2016-11-26 16:17:07 -0600

    updates to homepage

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,11 @@
 class Property < ActiveRecord::Base
-  has_many :alarms, :ciations
+  has_many :alarms
+  has_many :citations
   belongs_to :account_owner
+  validates :propertyName, format: { with: /\A[a-zA-Z]+\z/}
+  validates :address, :presence => true
+  validates :city, format: { with: /\A[a-zA-Z]+\z/}, :presence => true
+  validates :state, format: { with: /\A[a-zA-Z]+\z/}, length: { maximum: 2, minimum: 2}, :presence => true
+  validates :zip, numericality: true, length: {maximum:5, minimum: 5}, :presence => true
+
 end
